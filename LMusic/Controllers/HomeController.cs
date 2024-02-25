fd4282fbe9a82a0e8e8bcfa26a3dbae6eb127503
MashaKhor <144768937+MashaKhor@users.noreply.github.com>
2024-02-25 23:39:35 +0500

    addPanelAndProfile

--- LMusic/Controllers/HomeController.cs
+++ LMusic/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace LMusic.Controllers
             return View();
         }
 
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ LMusic/wwwroot/js/lorem.js
@@ -0,0 +1,39 @@
+﻿const arrowOpen = document.querySelector('.open-icon'),
+    arrowClose = document.querySelector('.close-icon'),
+    navigation = document.querySelector('.page-sidebar'),
+    sideItemSpan = navigation.querySelectorAll('.menu-link span'),
+    bodyContent = document.querySelector('.content-wrapper');
+
+function openLeftPanel() {
+    navigation.classList.add('active');
+    arrowOpen.classList.add('hidden');
+    arrowClose.classList.remove('hidden');
+    sideItemSpan.forEach(item => {
+        item.classList.remove('hidden');
+    });
+    bodyContent.style.width = `${document.documentElement.clientWidth - 230}px`;
+}
+
+function closeLeftPanel() {
+    navigation.classList.remove('active');
+    arrowOpen.classList.remove('hidden');
+    arrowClose.classList.add('hidden');
+    sideItemSpan.forEach(item => {
+        item.classList.add('hidden');
+    });
+    bodyContent.style.width = `${document.documentElement.clientWidth - 70}px`;
+}
+
+arrowOpen.addEventListener('click', openLeftPanel);
+arrowClose.addEventListener('click', closeLeftPanel);
+bodyContent.style.width = `${document.documentElement.clientWidth - 70}px`;
+
+const audio = document.querySelectorAll('.audio');
+console.log(audio);
+for (let i = 0; i < audio.length; i++) {
+    audio[i].addEventListener('ended', function () {
+        if (audio[i].duration === audio[i].currentTime) {
+            audio[i + 1].play();
+        }
+    });
+}
