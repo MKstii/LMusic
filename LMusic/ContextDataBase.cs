843fd834b6e5d20763fa84b851e45f827e596cdf
MKstii <101364255+MKstii@users.noreply.github.com>
2024-02-27 16:25:18 +0500

    Сделал базу InMemory

--- LMusic/ContextDataBase.cs
+++ LMusic/ContextDataBase.cs
@@ -4,6 +4,17 @@ namespace LMusic
 {
     public class ContextDataBase : DbContext
     {
+        public ContextDataBase() { }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("LMusic");
+            // Additional.DatabaseData будут объекты, которые надо будет засунуть в базу
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка моделей. Тут надо будет настроить 2 юзера
+        }
     }
 }
