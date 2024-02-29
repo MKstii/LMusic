37b65f28214363436adb698597b351db4437b7c8
MashaKhor <144768937+MashaKhor@users.noreply.github.com>
2024-02-29 21:50:35 +0500

    Пытаюсь исправить ошибку, что файлы не меняются

--- LMusic/wwwroot/js/lorem.js
+++ LMusic/wwwroot/js/lorem.js
@@ -1,39 +1,28 @@
-﻿const arrowOpen = document.querySelector('.open-icon'),
-    arrowClose = document.querySelector('.close-icon'),
+﻿const iconMenu = document.querySelector('.icon-menu'),
     navigation = document.querySelector('.page-sidebar'),
     sideItemSpan = navigation.querySelectorAll('.menu-link span'),
     bodyContent = document.querySelector('.content-wrapper');
 
-function openLeftPanel() {
-    navigation.classList.add('active');
-    arrowOpen.classList.add('hidden');
-    arrowClose.classList.remove('hidden');
+function toggleLeftMenu() {
+    navigation.classList.toggle('active');
     sideItemSpan.forEach(item => {
-        item.classList.remove('hidden');
+        item.classList.toggle('hidden');
     });
     bodyContent.style.width = `${document.documentElement.clientWidth - 230}px`;
 }
 
-function closeLeftPanel() {
-    navigation.classList.remove('active');
-    arrowOpen.classList.remove('hidden');
-    arrowClose.classList.add('hidden');
-    sideItemSpan.forEach(item => {
-        item.classList.add('hidden');
-    });
-    bodyContent.style.width = `${document.documentElement.clientWidth - 70}px`;
-}
+iconMenu.addEventListener('click', toggleLeftMenu);
 
-arrowOpen.addEventListener('click', openLeftPanel);
-arrowClose.addEventListener('click', closeLeftPanel);
 bodyContent.style.width = `${document.documentElement.clientWidth - 70}px`;
+console.log('AAA');
 
-const audio = document.querySelectorAll('.audio');
-console.log(audio);
-for (let i = 0; i < audio.length; i++) {
-    audio[i].addEventListener('ended', function () {
-        if (audio[i].duration === audio[i].currentTime) {
-            audio[i + 1].play();
-        }
-    });
-}
+
+//const audio = document.querySelectorAll('.audio');
+//console.log(audio);
+//for (let i = 0; i < audio.length; i++) {
+//    audio[i].addEventListener('ended', function () {
+//        if (audio[i].duration === audio[i].currentTime) {
+//            audio[i + 1].play();
+//        }
+//    });
+//}
