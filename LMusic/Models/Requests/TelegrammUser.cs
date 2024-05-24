50cd10f163a0408f15811b05ab5510e96d842c67
TomasHelby <valientin.rudin@mail.ru>
2024-05-24 22:24:16 +0500

    Поправка в конституции

--- LMusic/Models/Requests/TelegrammUser.cs
+++ LMusic/Models/Requests/TelegrammUser.cs
@@ -1,14 +1,16 @@
-﻿using System.Security.Cryptography.Xml;
+﻿using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.Xml;
 
 namespace LMusic.Models.Requests
 {
     public class TelegrammUser 
     {
         public int id { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string username { get; set; }
-        public string photo_url { get; set; }
+        public string? first_name { get; set; }
+        public string? last_name { get; set; }
+        public string? username { get; set; }
+        public string? photo_url { get; set; }
         public long auth_date { get; set; }
         public string hash { get; set; }
 
@@ -24,13 +26,30 @@ namespace LMusic.Models.Requests
             var res = new List<string>
             {
                 $"id={id}",
-                $"first_name={first_name}",
-                $"last_name={last_name}",
-                $"username={username}",
-                $"photo_url={photo_url}",
                 $"auth_date={auth_date}"
             };
+
+            res.AddIfNotNull("first_name",first_name);
+            res.AddIfNotNull("last_name", last_name);
+            res.AddIfNotNull("username", username);
+            res.AddIfNotNull("photo_url", photo_url);
+
             return res;
         }
+
+
     }
+
+    public static class ListExtension
+    {
+        public static void AddIfNotNull(this List<string> list, string name, object anyField)
+        {
+            if (anyField != null)
+            {
+                string str = $"{name}={anyField}";
+                list.Add(str);
+            }
+        }
+    }
+
 }
